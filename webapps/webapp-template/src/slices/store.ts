e14d3a408bbe3de884b8c51b713c70053bd0171e
dinethde <dinethran33@gmail.com>
2025-11-18 12:15:52 +0530

    fix: simplify middleware configuration in store setup

--- webapps/webapp-template/src/slices/store.ts
+++ webapps/webapp-template/src/slices/store.ts
@@ -36,9 +36,7 @@ export const store = configureStore({
     appConfig: appConfigReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      thunk: undefined,
-    }),
+    getDefaultMiddleware(),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
